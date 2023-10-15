24e32393f410134c6b54e688ae99bbd6172548a1
OlegVasil <100850508+OlegVasil@users.noreply.github.com>
2023-10-15 19:33:05 +0300

    БоЛьШе обработки возможных ошибок

--- reports.py
+++ reports.py
@@ -16,7 +16,7 @@ try:
     tasks_data = response_tasks.json()
     users_data = response_users.json()
 except requests.exceptions.RequestException as e:
-    print('Ошибка при выполнении HTTP-запроса: {e}')
+    print(f'Ошибка при выполнении HTTP-запроса: {e}')
     exit()
 
 # Создание директории
@@ -30,31 +30,46 @@ current_time = datetime.now(local_timezone)
 # Создания словаря для хранения данных отчетов в памяти
 reports_data = {}
 
-
 # Функция для создания текстового отчета для пользователя
 def create_report(user_data, tasks_data):
-    user_id = user_data['id']
-    username = user_data['username']
-    company_name = user_data['company']['name']
-    email = user_data['email']
-
     try:
-        # Формат имени файла
-        file_name = f'tasks/{username}.txt'
+        user_id = user_data.get('id')
+        username = user_data.get('username')
+        company_name = (user_data.get('company')).get('name')
+        email = user_data.get('email')
+    except TypeError as e:
+        print(f'user_data должен быть словарем: {e}')
 
-        # Проверка на существование отчета
-        if os.path.exists(file_name):
+    # Формат имени файла
+    file_name = f'tasks/{username}.txt'
+
+    # Проверка на существование отчета
+    if os.path.exists(file_name):
+        try:
             # Переименование существующего файла
             old_file_name = f'tasks/old_{username}_{current_time.strftime("%Y-%m-%dT%H:%M")}.txt'
             os.rename(file_name, old_file_name)
+        except OSError as e:
+            try:
+                old_file_name = f'tasks/old_{username}_{current_time.strftime("%Y-%m-%dTime%H_%M")}.txt'
+                os.rename(file_name, old_file_name)
+                print(f'Возможно вы используете не линукс? Выполнена обработка для Windows {e}')
+            except OSError as e:
+                print(f'Упс..., повторите попытку еще раз')
 
+
+    try:
         # Список для хранения данных отчета
         report_data = []
         report_data.append(f'# Отчёт для {company_name}.')
         report_data.append(f'{user_data["name"]} <{email}> {current_time.strftime("%d.%m.%Y %H:%M")}')
 
         # Фильтр задач по пользователю
-        user_tasks = [task for task in tasks_data if task.get('userId') == user_id]
+        try:
+            user_tasks = [task for task in tasks_data if task.get('userId') == user_id]
+        except TypeError as e:
+            print(f'task_data должен быть словарем: {e}')
+
         total_tasks = len(user_tasks)
 
         report_data.append(f'Всего задач: {total_tasks}\n')
@@ -74,13 +89,14 @@ def create_report(user_data, tasks_data):
             report_data.append('\n## Завершённые задачи ({0}):'.format(len(completed_tasks)))
             for task in completed_tasks:
                 task_title = task['title'][:46] + '...' if len(task['title']) > 46 else task['title']
-                report_data.append(f"- {task_title}")
+                report_data.append(f'- {task_title}')
 
-        # Сохранение отчета в словаре
-        reports_data[user_id] = '\n'.join(report_data)
+            # Сохранение отчета в словаре
+            #print(reports_data[user_id])
+            reports_data[user_id] = '\n'.join(report_data)
+
     except Exception as e:
-        print(f'Ошибка при выполнении отчета для пользователя {username}: {e}')
-
+        print(f'Непредвиденная ошибка при записи пользователя {username}: {e}')
 
 # Создание и запись отчета для всех существующих(непустых) пользователей
 for user in users_data:
@@ -89,9 +105,13 @@ for user in users_data:
     user_id = user['id']
     username = user['username']
 
-    # Получить отчет из словаря или пустую строку, если отчета нет
-    report_content = reports_data.get(user_id, '')
+    try:
+        # Получить отчет из словаря или пустую строку, если отчета нет
+        report_content = reports_data.get(user_id, '')
+    except TypeError as e:
+        print(f'reports_data должен быть словарем: {e}')
 
-    file_name = f'tasks/{username}.txt'
-    with open(file_name, 'w', encoding='utf-8') as file:
-        file.write(report_content)
+    if report_content:
+            file_name = f'tasks/{username}.txt'
+            with open(file_name, 'w', encoding='utf-8') as file:
+                file.write(report_content)
